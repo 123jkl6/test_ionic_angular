0f0cb94d9de312ecc2bd0b54d2196ea83bdbfd49
developer <nicholasngchuxu@gmail.com>
2019-08-16 00:06:14 +0800

    added delete function. While it works, going back to RecipesPage does not show change, will fix this by adding event listener in next commit.

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -14,6 +14,7 @@ export class RecipesPage implements OnInit {
   constructor(private recipesService: RecipesService) {}
 
   ngOnInit() {
+    console.log("ngOnInit() RecipesPage");
     this.recipes = this.recipesService.getAllRecipes();
   }
 }
